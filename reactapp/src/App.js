c7261bf40150f65b05589e02a84e783e3b3b7b50
Nishar <mohamednisharj@gmail.com>
2025-09-08 18:40:29 +0530

    login import is changed

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import ApplyForm from './components/ApplyForm';
-import RegisterForm from './components/RegisterForm';  // ✅ fixed import name
+import RegisterForm from './components/RegisterForm'; 
 import DisplayHealthCoach from './components/DisplayHealthCoach';
 import DisplayClients from './components/DisplayClients';
 import Footer from './components/Footer';
 import Notifications from './components/Notification';
 import ClientRequestForm from './components/ClientRequestForm';
 import CoachRequestForm from './components/CoachRequestForm';
-import LoginPage from './components/Login';        
+import Login from './components/Login.jsx';        
 import PrivateRoute from './components/PrivateRoute';
 
 function App() {
@@ -29,7 +29,7 @@ function App() {
 
           {/* Public Routes */}
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<LoginPage />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/apply" element={<ApplyForm />} />
           <Route path="/register" element={<RegisterForm />} />
           <Route path="/getAllCoaches" element={<DisplayHealthCoach />} />
